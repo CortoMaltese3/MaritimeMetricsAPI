f5f816f21f769dfa1768a0d81651255c0765e4ed
CortoMaltese3 <giorgos.kalomalos@outlook.com>
2024-03-23 21:18:50 +0200

    feat/Add sphinx documentation support
    - Add basic sphinx setup files
    - Add sphinx and sphinx-rtd-theme in requirements.txt
    - Various changes to comform to the sphinx docstring standards

--- app/views.py
+++ app/views.py
@@ -31,19 +31,20 @@ def get_vessel_invalid_data(vessel_code: str) -> Response:
     :return: A JSON response containing a summary of invalid data issues.
     :rtype: Response
 
-    Example response:
-    ```
-    {
-        "message": "Found invalid data for this vessel",
-        "vessel_code": 3001,
-        "invalid_data": {
-            "below_zero": {
-                "proposed_speed_overground": 30686,
-                "predicted_fuel_consumption": 1669,
-                "power": 1375
-            },
-            ...
-    ```
+    Example response::
+
+            {
+                "message": "Found invalid data for this vessel",
+                "vessel_code": 3001,
+                "invalid_data": {
+                    "below_zero": {
+                        "proposed_speed_overground": 30686,
+                        "predicted_fuel_consumption": 1669,
+                        "power": 1375
+                    }
+                    ...
+                }
+            }
     """
     try:
         vessel_code_int = int(vessel_code)
@@ -90,21 +91,20 @@ def get_vessel_speed_difference(vessel_code: str) -> Response:
     :return: A JSON response containing the speed differences for the vessel.
     :rtype: Response
 
-    Example response:
-    ```
-    {
-        "message": "Speed differences for the vessel",
-        "vessel_code": 19310,
-        "speed_differences": [
+    Example response::
+
             {
-                "latitude": 49.2837677001953,
-                "longitude": -123.177825927734,
-                "speed_difference": 0.747206647694111
-            },
-            ...
-        ]
-    }
-    ```
+                "message": "Speed differences for the vessel",
+                "vessel_code": 19310,
+                "speed_differences": [
+                    {
+                        "latitude": 49.2837677001953,
+                        "longitude": -123.177825927734,
+                        "speed_difference": 0.747206647694111
+                    }
+                    ...
+                ]
+            }
     """
     try:
         vessel_code_int = int(vessel_code)
@@ -152,13 +152,12 @@ def vessel_compliance_comparison(vessel_code1: str, vessel_code2: str) -> Respon
     :return: A JSON response indicating the comparison result.
     :rtype: Response
 
-    Example response:
-    ```
-    {
-        "message": "Vessel 19310 is more compliant with a compliance score of 83.54%
-                    compared to Vessel 3001's score of 72.11%."
-    }
-    ```
+    Example response::
+
+            {
+                "message": "Vessel 19310 is more compliant with a compliance score of 83.54%
+                            compared to Vessel 3001's score of 72.11%."
+            }
     """
     try:
         vessel_code1_int = int(vessel_code1)
@@ -188,24 +187,23 @@ def get_vessel_metrics(vessel_code: str, start_date: str, end_date: str) -> Resp
     :return: A JSON response containing the metrics for the specified vessel and period.
     :rtype: Response
 
-    Example response:
-    ```
-    [
-        {
-            "vessel_code": 3001,
-            "datetime": "2023-06-01 00:01:00",
-            "latitude": 10.2894496918,
-            "longitude": -14.7888498306,
-            "power": 0.0,
-            "fuel_consumption": 0.0,
-            "actual_speed_overground": 0.09999,
-            "proposed_speed_overground": 0.9464979896,
-            "predicted_fuel_consumption": 0.0,
-            "speed_difference": 0.8465079896
-        },
-      ...
-    ]
-    ```
+    Example response::
+
+            [
+                {
+                    "vessel_code": 3001,
+                    "datetime": "2023-06-01 00:01:00",
+                    "latitude": 10.2894496918,
+                    "longitude": -14.7888498306,
+                    "power": 0.0,
+                    "fuel_consumption": 0.0,
+                    "actual_speed_overground": 0.09999,
+                    "proposed_speed_overground": 0.9464979896,
+                    "predicted_fuel_consumption": 0.0,
+                    "speed_difference": 0.8465079896
+                }
+                ...
+            ]
     """
     try:
         vessel_code_int = int(vessel_code)
@@ -246,24 +244,25 @@ def get_vessel_raw_metrics(
     :return: A JSON response containing the raw data for the vessel within the specified period.
     :rtype: Response
 
-    Example response:
-    ```
-    [
-        {
-            "vessel_code": 3001,
-            "datetime": "2023-06-01 00:00:00",
-            "latitude": 10.2894458771,
-            "longitude": -14.7888755798,
-            "power": 0.0,
-            "fuel_consumption": 0.0,
-            "actual_speed_overground": 0.039996,
-            "proposed_speed_overground": -0.1899042625,
-            "predicted_fuel_consumption": 0.0
-        },
-      ...
-    ]
-    ```
+    Example response::
+
+
+            [
+                {
+                    "vessel_code": 3001,
+                    "datetime": "2023-06-01 00:00:00",
+                    "latitude": 10.2894458771,
+                    "longitude": -14.7888755798,
+                    "power": 0.0,
+                    "fuel_consumption": 0.0,
+                    "actual_speed_overground": 0.039996,
+                    "proposed_speed_overground": -0.1899042625,
+                    "predicted_fuel_consumption": 0.0
+                }
+                ...
+            ]
     """
+
     try:
         vessel_code_int = int(vessel_code)
     except ValueError:
